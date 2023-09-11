504a302d5a1715a193639cfd9893189b8ea4e639
Maxim Sokov <sokovm@mail.ru>
2023-09-11 15:30:54 +0530

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button. addEventListener( 'click', function(){
+  output. innerHTML = 'ПОМНИ !!!!! , ты кушаешь два раза в день!!! Между приёмами пищи, можешь пить зелёный чай без сахара!!!!!        Ты большая молодец, очень мужественно сражаешься с наркотой, я тобой горжусь ))!!!!' ;
+}
+);
